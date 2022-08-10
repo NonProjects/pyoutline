8badccf6c5320a592825c77dccf94cfbb2f07f70
NotStatilko <thenonproton@protonmail.com>
2022-08-10 03:52:48 +0300

    Prepare setup.py for v0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pyoutline",
-    version='0.2',
+    version='0.2.1',
     license='MIT',
     description = 'A simple CLI app to start Outline Proxy',
     long_description = open('README.md').read(),
@@ -10,7 +10,7 @@ setup(
     author = 'NonProjects',
     author_email = 'thenonproton@pm.me',
     url = 'https://github.com/NonProjects/pyoutline',
-    download_url = 'https://github.com/NonProjects/pyoutline/archive/refs/tags/v0.2.tar.gz',
+    download_url = 'https://github.com/NonProjects/pyoutline/archive/refs/tags/v0.2.1.tar.gz',
     py_modules=['pyoutline', 'tools'],
     install_requires=['click'],
     entry_points='''
