0d2f14a7fe4b61ce31b0e685bfbe3db14aa4f8a7
NotStatilko <thenonproton@protonmail.com>
2022-10-11 20:37:25 +0300

    Remove install from third party. PyPi doesn't allow it

--- setup.py
+++ setup.py
@@ -1,24 +1,18 @@
 from setuptools import setup
 
-# github.com/shadowsocks/shadowsocks commit from master. For safety.
-ss_commit = '5ff694b2c2978b432918dea6ac104706b25cbf48'
-ss_archive = f'https://github.com/shadowsocks/shadowsocks/archive/{ss_commit}.zip'
-
 setup(
     name="pyoutline",
-    version='0.3',
+    version='0.3.1',
     license='MIT',
-    description = 'A simple CLI app to start Outline Proxy',
+    description = 'A simple CLI app to run Outline VPN',
     long_description = open('README.md').read(),
     long_description_content_type='text/markdown',
     author = 'NonProjects',
     author_email = 'thenonproton@pm.me',
     url = 'https://github.com/NonProjects/pyoutline',
-    download_url = 'https://github.com/NonProjects/pyoutline/archive/refs/tags/v0.3.tar.gz',
+    download_url = 'https://github.com/NonProjects/pyoutline/archive/refs/tags/v0.3.1.tar.gz',
     py_modules=['pyoutline', 'pyoutline_tools'],
-    install_requires=[
-        'click', f'shadowsocks @ {ss_archive}'
-    ],
+    install_requires=['click'],
     entry_points='''
         [console_scripts]
         pyoutline=pyoutline:safe_cli
