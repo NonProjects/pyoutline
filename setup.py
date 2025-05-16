1b086b142beb945341d80ae796d533a974863f50
NotStatilko <thenonproton@protonmail.com>
2025-05-16 12:35:23 +0300

    Bump version to v0.4

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup
 
 setup(
     name="pyoutline",
-    version='0.3.1',
+    version='0.4',
     license='MIT',
-    description = 'A simple CLI app to run Outline VPN',
+    description = 'A simple CLI app to run Outline VPN keys',
     long_description = open('README.md').read(),
     long_description_content_type='text/markdown',
     author = 'NonProjects',
     author_email = 'thenonproton@pm.me',
     url = 'https://github.com/NonProjects/pyoutline',
-    download_url = 'https://github.com/NonProjects/pyoutline/archive/refs/tags/v0.3.1.tar.gz',
+    download_url = 'https://github.com/NonProjects/pyoutline/archive/refs/tags/v0.4.tar.gz',
     py_modules=['pyoutline', 'pyoutline_tools'],
-    install_requires=['click'],
+    install_requires=['click', 'nonshadowsocks'],
     entry_points='''
         [console_scripts]
         pyoutline=pyoutline:safe_cli
